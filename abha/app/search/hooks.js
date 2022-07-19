f16a0b3be001490ca1c93323a12994b829a7e328
srushti.raybhoge <srushti.raybhoge@bfhl.in>
2022-07-19 11:05:06 +0530

    comments removed

--- abha/app/search/hooks.js
+++ abha/app/search/hooks.js
@@ -1,11 +1,6 @@
 import {useQuery} from 'react-query';
 import axios from 'axios';
 
-// const fetchSearchResults = async () => {
-//     const { data } = await axios.get('https://bfhldevapigw.healthrx.co.in/labs/v1/admin/provider');
-//     return data;
-// };
-
 export const fetchSearchResultsQuery = async data => {
   return axios.post(
     'https://bfhldevapigw.healthrx.co.in/uhi-intelligent-search/send-suggestion',
@@ -18,7 +13,3 @@ export const fetchSelectedResultsQuery = async data => {
     data,
   );
 };
-
-// const useSearchResults = () => useQuery('posts', ()=>fetchSearchResultsQuery());
-
-// export default fetchSearchResultsQuery;
